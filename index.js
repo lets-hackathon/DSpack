2ed2d368460cec8e0158b733419bb25295f5aba1
Kanishk Gupta <kanishkgupta2000@gmail.com>
2019-10-16 00:29:39 +0530

    add graph functionality
    Signed-off-by: Kanishk Gupta <kanishkgupta2000@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const LinkedList = require("./data-structures/linkedList");
 const Queue = require("./data-structures/Queue");
 const Stack = require("./data-structures/Stack");
+const Graph = require("./data-structures/Graph");
+
 
 module.exports.LinkedList = LinkedList;
 module.exports.Queue = Queue;
 module.exports.Stack = Stack;
+module.exports.Graph = Graph;
+
